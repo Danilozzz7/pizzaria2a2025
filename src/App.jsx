5ca2fc52080d3f2c9bc789fd4c53385e18cdbca3
Danilozzz7git config --global user.email <rm93961@estudante.fieb.edu.br>
2025-04-02 08:14:50 -0300

    passagem de props

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ function App(){
 
   const [nome, setNome] = useState('Ronaldo');
 
-  const Formulario = () => {
+  const Formulario = (props) => {
     return (
       <>
-      <h4>Formulário de Inscrição</h4>
-      <input className="Nome"
-      name = "nome"
-      placeholder="Digite seu nome..."
-      onChange={(e)=>setNome(e.target.value)}
-          type="text" />
-      <button onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-      </button>
+        <h4>Formulário de Inscrição</h4>
+        <input className="Nome"
+            name = "nome"
+            placeholder="Digite seu nome..."
+            onChange={(e)=>props.nome = e.target.value}
+            type="text" />
+        <button onClick={()=>{
+         (props.texto != null) ? alert(props.texto) : alert(props.nome)
+          }}>
+          CLIQUE AQUI
+        </button>
       </>
     
     )
@@ -24,9 +26,10 @@ function App(){
   return (
     <div>
       <h3>Pizzaria 2A</h3>
-      <Formulario />
-      <Formulario />
-      <Formulario />
+      <Formulario texto="Texto enviado pelo App" />
+      <Formulario texto="São paulo meu amor" />
+      <Formulario texto="Novo texto enviado pelo App" />
+      <Formulario nome={nome} />
       
       
     </div>
